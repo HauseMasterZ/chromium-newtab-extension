250aa660ff0452f455f2b5b4c359487a94305401
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2023-02-12 20:46:50 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+// function enter() {
+//     let text = document.getElementById("input-id").value;
+//     if (!text.trim().length) {
+//         document.getElementById("m-form").reset();
+//         return 1;
+//     }
+// }
+document.body.style.zoom = "110%";
